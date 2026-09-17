e0b0b46c45dd1bd104792747dfcde03605f87338
Ernesto Gonzalez <ernesto.go.noch@gmail.com>
2026-09-16 21:43:53 -0600

    feat: add application entry point

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vtqp_flutter_gym_sample/app/app.dart';
+
+void main() {
+  runApp(const ProviderScope(child: GymSampleApp()));
+}
